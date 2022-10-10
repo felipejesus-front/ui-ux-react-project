fc4207f3bd0ea02821be0dc4cb00355cd7bec938
Felipe Campos <felipe.campos@al.resilia.com.br>
2022-10-10 11:55:47 -0300

    consertando bug do caminho das imagens

--- src/Utils/Objects.tsx
+++ src/Utils/Objects.tsx
@@ -2,61 +2,61 @@ export const Objects = {
 	people: [
 		{
 			name: "Brendo Simpsom",
-			avatar: "/icon-list/avatar1.png",
+			avatar: "npoicon-list/avatar1.png",
 			ranking: 1,
 			atualUser: false,
 		},
 		{
 			name: "Angela Bower",
-			avatar: "/icon-list/avatar2.png",
+			avatar: "icon-list/avatar2.png",
 			ranking: 2,
 			atualUser: false,
 		},
 		{
 			name: "Lynn Tanner",
-			avatar: "/icon-list/avatar3.png",
+			avatar: "icon-list/avatar3.png",
 			ranking: 3,
 			atualUser: false,
 		},
 		{
 			name: "Dr. Bonnie Barstow",
-			avatar: "/icon-list/avatar4.png",
+			avatar: "icon-list/avatar4.png",
 			ranking: 4,
 			atualUser: false,
 		},
 		{
 			name: "Thomas Magnum",
-			avatar: "/icon-list/avatar5.png",
+			avatar: "icon-list/avatar5.png",
 			ranking: 5,
 			atualUser: true,
 		},
 		{
 			name: "B.A. Baracus",
-			avatar: "/icon-list/avatar6.png",
+			avatar: "icon-list/avatar6.png",
 			ranking: 6,
 			atualUser: false,
 		},
 		{
 			name: "Lynn Tanner",
-			avatar: "/icon-list/avatar7.png",
+			avatar: "icon-list/avatar7.png",
 			ranking: 7,
 			atualUser: false,
 		},
 		{
 			name: "Col. Roderick Decker",
-			avatar: "/icon-list/avatar8.png",
+			avatar: "icon-list/avatar8.png",
 			ranking: 8,
 			atualUser: false,
 		},
 		{
 			name: "April Curtis",
-			avatar: "/icon-list/avatar9.png",
+			avatar: "icon-list/avatar9.png",
 			ranking: 9,
 			atualUser: false,
 		},
 		{
 			name: "wirley Johson",
-			avatar: "/icon-list/avatar1.png",
+			avatar: "icon-list/avatar1.png",
 			ranking: 10,
 			atualUser: false,
 		},
@@ -64,21 +64,21 @@ export const Objects = {
 	certificarionsOwned: [
 		{
 			title: "Intro to I.O.T",
-			img: "/cert1.jpg",
+			img: "cert1.jpg",
 		},
 		{
 			title: "Project Management",
-			img: "/cert2.png",
+			img: "cert2.png",
 		},
 	],
 	avaibleCertifications: [
 		{
 			title: "Finanancial Management",
-			img: "/cert3.jpg",
+			img: "cert3.jpg",
 		},
 		{
 			title: "Planning",
-			img: "/cert4.jpg",
+			img: "cert4.jpg",
 		},
 	],
 };
